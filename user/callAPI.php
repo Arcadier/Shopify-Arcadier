a04be4419402541d24ab1aded55e366cda44a506
Natasha Maruska Navarro <nmfnavarro@gmail.com>
2022-08-07 04:32:11 +0800

    modify authorization - authentication
    added 2 fields on auth custom table (api_key, secret_key)
    where the obtained token should be stored as well upon valid authentication

--- user/callAPI.php
+++ user/callAPI.php
@@ -37,25 +37,25 @@ function callAPI($method, $access_token, $url, $data = false) {
     return json_decode($result, true);
 }
 
-function getMarketplaceBaseUrl() {
-    $marketplace = $_COOKIE["marketplace"];
-    $protocol = $_COOKIE["protocol"];
+// function getMarketplaceBaseUrl() {
+//     $marketplace = $_COOKIE["marketplace"];
+//     $protocol = $_COOKIE["protocol"];
 
-    $baseUrl = $protocol . '://' . $marketplace;
-    return $baseUrl;
-}
+//     $baseUrl = $protocol . '://' . $marketplace;
+//     return $baseUrl;
+// }
 
-function getPackageID() {
-    $requestUri = "$_SERVER[REQUEST_URI]";
-    preg_match('/([a-f0-9]{8}(?:-[a-f0-9]{4}){3}-[a-f0-9]{12})/', $requestUri, $matches, 0);
-    return $matches[0];
-}
+// function getPackageID() {
+//     $requestUri = "$_SERVER[REQUEST_URI]";
+//     preg_match('/([a-f0-9]{8}(?:-[a-f0-9]{4}){3}-[a-f0-9]{12})/', $requestUri, $matches, 0);
+//     return $matches[0];
+// }
 
-function getCustomFieldPrefix() {
-    $requestUri = "$_SERVER[REQUEST_URI]";
-    preg_match('/([a-f0-9]{8}(?:-[a-f0-9]{4}){3}-[a-f0-9]{12})/', $requestUri, $matches, 0);
-    $customFieldPrefix = str_replace('-', '', $matches[0]);
-    return $customFieldPrefix;
-}
+// function getCustomFieldPrefix() {
+//     $requestUri = "$_SERVER[REQUEST_URI]";
+//     preg_match('/([a-f0-9]{8}(?:-[a-f0-9]{4}){3}-[a-f0-9]{12})/', $requestUri, $matches, 0);
+//     $customFieldPrefix = str_replace('-', '', $matches[0]);
+//     return $customFieldPrefix;
+// }
 
 ?>
