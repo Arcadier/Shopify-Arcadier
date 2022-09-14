e2c0890eb9c9e166b54a315c9c312958090be0d2
Tanoo <tanoo@arcadier.com>
2022-09-14 12:05:19 +0800

    completed webhooks for items

--- user/callAPI.php
+++ user/callAPI.php
@@ -40,6 +40,47 @@ function callAPI($method, $access_token, $url, $data = false) {
     
 }
 
+function shopifyAPI($method, $access_token, $url, $data = false) {
+    $curl = curl_init();
+    switch ($method) {
+        case "POST":
+            curl_setopt($curl, CURLOPT_POST, 1);
+            if ($data) {
+                $jsonDataEncoded = json_encode($data);
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+            }
+            break;
+        case "PUT":
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
+            if ($data) {
+                $jsonDataEncoded = json_encode($data);
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+            }
+            break;
+        case "DELETE":
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'DELETE');
+            break;
+        default:
+            if ($data) {
+                $url = sprintf("%s?%s", $url, http_build_query($data));
+            }
+    }
+    $headers = ['Content-Type: application/json'];
+    if ($access_token != null && $access_token != '') {
+        array_push($headers, sprintf('X-Shopify-Access-Token: %s', $access_token));
+    }
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    
+    $result = curl_exec($curl);
+    
+    curl_close($curl);
+    
+    return json_decode($result, true);
+    
+}
+
 function getInstallURL($method, $access_token, $url, $data = false) {
     $curl = curl_init();
     switch ($method) {
